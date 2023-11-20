9cd75e9bcde5eac9f1bc124473d1f02e5518a887
eshrat <eshratjahan701@gmail.com>
2023-11-20 08:10:03 +0600

    1st commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Todo = () => {
+    const [inputTask, setInputTask] = useState('');
+    const [inputDesc, setInputDesc] = useState('');
     return (
         <div>
             <h1 className='text-3xl text-white font-bold text-center mt-5'>My Todos</h1>
             <div className='flex items-center justify-center m-5' >
                 <div className='mr-2'>
                     
-                    <input type="text" placeholder="Task Name " className="input input-bordered w-full max-w-xs " />
+                    <input type="text" placeholder="Task Name "
+                        onChange={e=>{setInputTask(e.target.value)}}
+                    
+                        className="input input-bordered w-full max-w-xs " />
 
                 </div>
                 <div className='mr-2'>
                     
-                    <input type="text" placeholder="Description" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Description"
+                        onChange={e=>{setInputDesc(e.target.value)}}
+                        className="input input-bordered w-full max-w-xs" />
                 </div>
                 <div>
-                <button className="btn btn-warning rounded-full font-bold ">Add task</button>
+                    <button className="btn btn-warning rounded-full font-bold ">Add task</button>
+                    <div>{inputTask}</div>
+                    <div>{inputDesc}</div>
                 </div>
         </div>
         </div>
